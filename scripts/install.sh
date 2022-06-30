ed915bc24874571b66ac082cc227621e93502e03
thegergo02 <work@varigergo.xyz>
2022-06-30 18:10:46 +0200

    add: copy hardware conf to etc (temporary)

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,6 +8,7 @@ echo "Generating hardware-configuration.nix for $HOST..."
 nixos-generate-config --root $ROOT --show-hardware-config >> system/$HOST/hardware-configuration.nix
 echo "Installing NixOS on $HOST..."
 nixos-install --root $ROOT --flake .#$HOST
+cp system/$HOST/hardware-configuration.nix /mnt/etc # FIXME: temporary, should push to repo
 echo "Don't forget to change user passwords!"
 echo "Printing users:"
 awk -F: '($3>=1000)&&($1!="nobody")&&($1!~/nixbld*/){print $1}' $ROOT/etc/passwd
