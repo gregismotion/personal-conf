e54e3705fcbf422a4b9dd11532e401feff68a9ec
thegergo02 <work@varigergo.xyz>
2022-06-30 17:49:02 +0200

    fix: new path

--- system/installer/configuration.nix
+++ system/installer/configuration.nix
@@ -5,7 +5,7 @@
     ../common/flakes.nix
     ../common/basic.nix
     ../common/get-conf.nix
-    ../common/debug-ssh.nix
+    ../common/services/debug-ssh.nix
   ];
   
   system.stateVersion = "21.11";
