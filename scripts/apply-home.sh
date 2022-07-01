d2f2ec8ea7ec8b81ad1b5020e1fd434f4794cacc
thegergo02 <work@varigergo.xyz>
2022-07-01 08:38:20 +0200

    feat: switch home manager mode

--- scripts/apply-home.sh
+++ scripts/apply-home.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 pushd $(dirname $0)/..
+pushd users/$USER/
+if [[ $CONF_HEADLESS != 0 ]] 
+then
+	ln -sf headless.nix home.nix
+else
+	ln -sf gui.nix home.nix
+fi
+popd
 nix build .#homeManagerConfigurations.$USER.activationPackage
 ./result/activate
 popd
