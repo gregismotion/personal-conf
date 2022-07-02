c6c70679c74e2c5b120ec1fbe5ade8fdc5cf0860
thegergo02 <work@varigergo.xyz>
2022-07-02 08:59:53 +0200

    fix: when user has no conf

--- scripts/apply-home.sh
+++ scripts/apply-home.sh
@@ -3,18 +3,20 @@ pushd $(dirname $0)/..
 if [[ ! -f "$HOME/.config/git/config" ]]; then
 	git config --global --add safe.directory "$(pwd)"
 fi
-pushd users/$USER/
-if [[ $CONF_HEADLESS != 0 ]] 
-then
-	ln -sf headless.nix home.nix
-else
-	ln -sf gui.nix home.nix
+if [[ -d "users/$USER/" ]]; then # NOTE: fix when user has no config
+	pushd users/$USER/
+	if [[ $CONF_HEADLESS != 0 ]] 
+	then
+		ln -sf headless.nix home.nix
+	else
+		ln -sf gui.nix home.nix
+	fi
+	if [[ $USER -eq "root" ]]
+	then
+		chown root:conf home.nix # NOTE: fix when root runs script
+	fi
+	popd
+	nix build .#homeManagerConfigurations.$USER.activationPackage
+	./result/activate
 fi
-if [[ $USER -eq "root" ]]
-then
-	chown root:conf home.nix # NOTE: fix when root runs script
-fi
-popd
-nix build .#homeManagerConfigurations.$USER.activationPackage
-./result/activate
 popd
