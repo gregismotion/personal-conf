9a899edc7ebd0cd3da9956c36c4ae090dd05ddd0
thegergo02 <work@varigergo.xyz>
2022-07-02 12:23:38 +0200

    fix: some touchups

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,20 +6,19 @@ HOST=${1:-kyrios}
 ROOT=${2:-/setup}
 if [[ -d "$ROOT" ]]; then
 	echo "$ROOT is created, assuming manual installation..."
-	MANUAL=1
 else
-	echo "Missing system root, trying 'setup.sh'..."
+	echo "Missing system root, trying 'setup.sh'."
 	SETUP=system/$HOST/setup.sh
 	if [[ -f "$SETUP" ]]; then
+		echo "Running '$SETUP'..."
 		$SETUP
-		MANUAL=0
 	else
 		echo "No setup.sh, aborting!"
 		exit 1
 	fi
 fi
 if [[ -d "system/$HOST" ]]; then
-	echo "Generating hardware-configuration.nix for $HOST..."
+	echo "Generating hardware-configuration.nix for $HOST."
 	HARDWARE_CONF=system/$HOST/hardware-configuration.nix
 	if [[ -f "$HARDWARE_CONF" ]]; then
 	rm $HARDWARE_CONF
@@ -27,22 +26,14 @@ if [[ -d "system/$HOST" ]]; then
 	nixos-generate-config --root $ROOT --show-hardware-config >> system/$HOST/hardware-configuration.nix
 
 	echo "Installing NixOS on $HOST..."
-	if [[ $MANUAL != 0 ]]; then
-		echo "Manual install: will be prompted for root password."
-		nixos-install --root $root --flake .#$HOST
-	else
-		nixos-install --no-root-passwd --root $root --flake .#$HOST
-	fi
+	nixos-install --root $root --flake .#$HOST
+	
+	echo "Fixing permissions on config directory."
 	cp -r $(dirname $0)/.. $ROOT/etc/nixos
 	chown -R root:conf $ROOT/etc/nixos
 	chmod -R g+rwx $ROOT/etc/nixos
 
 	# TODO: push hardware conf to repo
-
-	echo "Don't forget to change user passwords!"
-	echo "Printing users:"
-	awk -F: '($3>=1000)&&($1!="nobody")&&($1!~/nixbld*/){print $1}' $ROOT/etc/passwd
-	echo "Use 'passwd <user>'! (After booting into system.)"
 
 	echo "NixOS is installed and configured on $HOST."
 	echo "Veni, vidi, vici."
