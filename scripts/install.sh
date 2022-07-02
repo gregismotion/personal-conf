9aabfd05ac7cb320975e13eb11c729999a1502f2
thegergo02 <work@varigergo.xyz>
2022-07-02 11:56:29 +0200

    fix: checks

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,18 +4,34 @@ set -e
 pushd $(dirname $0)/..
 HOST=${1:-kyrios}
 ROOT=${2:-/mnt}
-echo "Generating hardware-configuration.nix for $HOST..."
-nixos-generate-config --root $ROOT --show-hardware-config >> system/$HOST/hardware-configuration.nix
-echo "Installing NixOS on $HOST..."
-nixos-install --root $ROOT --flake .#$HOST
-cp -r $(dirname $0)/.. /mnt/etc/nixos
-chown -R root:conf /mnt/etc/nixos
-chmod -R g+rwx /mnt/etc/nixos
-# TODO: push hardware conf to repo
-echo "Don't forget to change user passwords!"
-echo "Printing users:"
-awk -F: '($3>=1000)&&($1!="nobody")&&($1!~/nixbld*/){print $1}' $ROOT/etc/passwd
-echo "Use 'passwd <user>'! (After booting into system.)"
-echo "NixOS is installed and configured on $HOST."
-echo "Veni, vidi, vici."
+if [[ -d "$ROOT" ]]; then
+	if [[ -d "system/$HOST" ]]; then
+		echo "Generating hardware-configuration.nix for $HOST..."
+		HARDWARE_CONF=system/$HOST/hardware-configuration.nix
+		if [[ -f "$HARDWARE_CONF" ]]; then
+			rm $HARDWARE_CONF
+		fi
+		nixos-generate-config --root $ROOT --show-hardware-config >> system/$HOST/hardware-configuration.nix
+
+		echo "Installing NixOS on $HOST..."
+		nixos-install --root $ROOT --flake .#$HOST
+		cp -r $(dirname $0)/.. $ROOT/etc/nixos
+		chown -R root:conf $ROOT/etc/nixos
+		chmod -R g+rwx $ROOT/etc/nixos
+
+		# TODO: push hardware conf to repo
+
+		echo "Don't forget to change user passwords!"
+		echo "Printing users:"
+		awk -F: '($3>=1000)&&($1!="nobody")&&($1!~/nixbld*/){print $1}' $ROOT/etc/passwd
+		echo "Use 'passwd <user>'! (After booting into system.)"
+
+		echo "NixOS is installed and configured on $HOST."
+		echo "Veni, vidi, vici."
+	else
+		echo "Missing host configuration!"
+	fi
+else
+	echo "Missing system root!"
+fi
 popd
