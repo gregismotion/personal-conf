be9a1a83637ab9c6832406a359930baf4aa568ac
thegergo02 <work@varigergo.xyz>
2022-07-03 16:19:57 +0200

    fix: proper perms

--- system/common/key.nix
+++ system/common/key.nix
@@ -10,8 +10,14 @@
   
   age.identityPaths = [ "/etc/nixos/keys/${config.networking.hostName}/id_rsa" ];
   
-  environment.etc."ssh/id_rsa".source = 
+  environment.etc."ssh/id_rsa" = {
+    mode = "0600";
+    source = 
     "${inputs.keys}/${config.networking.hostName}/id_rsa_secret";
-  environment.etc."ssh/id_rsa.pub".source = 
+  }
+  environment.etc."ssh/id_rsa.pub" = {
+    mode = "0600";
+    source = 
     "${inputs.keys}/${config.networking.hostName}/id_rsa_secret.pub";
+  }
 }
