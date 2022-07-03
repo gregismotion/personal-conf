98c3b2e97e860915ba0c3704bc5ff2846ab4d5bf
thegergo02 <work@varigergo.xyz>
2022-07-03 11:55:09 +0200

    refactor: naming

--- system/common/key.nix
+++ system/common/key.nix
@@ -4,10 +4,8 @@
   environment.etc."ssh/ssh_known_hosts".source = 
     "${inputs.keys}/${config.networking.hostName}/ssh_known_hosts";
 
-  environment.etc."ssh/id_rsa_secret".source = 
-    "${inputs.keys}/${config.networking.hostName}/id_rsa_secret";
-  environment.etc."ssh/id_rsa_secret.pub".source = 
-    "${inputs.keys}/${config.networking.hostName}/id_rsa_secret.pub";
-
-  environment.etc."ssh/id_rsa".target = "id_rsa_secret";
+  environment.etc."ssh/id_rsa".source = 
+    "${inputs.keys}/${config.networking.hostName}/id_rsa";
+  environment.etc."ssh/id_rsa.pub".source = 
+    "${inputs.keys}/${config.networking.hostName}/id_rsa.pub";
 }
