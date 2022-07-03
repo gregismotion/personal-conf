a9f01c0ebd4789d044ebc8b0cb2d60a2502d29be
thegergo02 <work@varigergo.xyz>
2022-07-03 16:28:38 +0200

    refactor: isolate root

--- system/common/services/debug-ssh.nix
+++ system/common/services/debug-ssh.nix
@@ -3,7 +3,4 @@
 {
   services.sshd.enable = true;
   services.openssh.permitRootLogin = lib.mkDefault "yes";
-
-  users.users.root.password = "nixos";
-  services.getty.autologinUser = lib.mkDefault "root";
 }
