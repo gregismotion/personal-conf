66e806e5f10d8f672a5ccfc84e01ae3eac1d0968
thegergo02 <work@varigergo.xyz>
2022-07-03 11:36:49 +0200

    fix: add known hosts and symlink

--- system/common/key.nix
+++ system/common/key.nix
@@ -1,8 +1,13 @@
 { config, inputs, ...  }:
 
 {
+  environment.etc."ssh/ssh_known_hosts".source = 
+    "${inputs.keys}/${config.networking.hostName}/ssh_known_hosts";
+
   environment.etc."ssh/id_rsa_secret".source = 
     "${inputs.keys}/${config.networking.hostName}/id_rsa_secret";
   environment.etc."ssh/id_rsa_secret.pub".source = 
     "${inputs.keys}/${config.networking.hostName}/id_rsa_secret.pub";
+
+  environment.etc."ssh/id_rsa".target = "id_rsa_secret";
 }
