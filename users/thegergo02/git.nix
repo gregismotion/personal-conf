770b6d19f9d0eba4a49cae629cad36846bf905d9
thegergo02 <work@varigergo.xyz>
2022-07-04 11:52:53 +0200

    fix: hm isolation

---
+++ users/thegergo02/git.nix
@@ -0,0 +1,15 @@
+{ pkgs, ...  }:
+
+{
+    programs.git = {
+      enable = true;
+      userName = "thegergo02";
+      userEmail = "work@varigergo.xyz";
+      extraConfig = {
+        init.defaultBranch = "master";
+        url."github:".insteadOf = "https://github.com/";
+        url."freeself_git:".insteadOf = "https://git.freeself.one/";
+        safe.directory = "*";
+      };
+    };
+}
---
+++ users/thegergo02/zsh.nix
@@ -0,0 +1,30 @@
+{ pkgs, ...  }:
+
+{
+  programs.zsh = {
+    enable = true;
+    enableAutosuggestions = true;
+    enableCompletion = true;
+    plugins = [
+      {
+        name = "pure";
+        src = pkgs.fetchFromGitHub {
+          owner = "sindresorhus";
+          repo = "pure";
+          rev  = "v1.20.1";
+          sha256 = "1bxg5i3a0dm5ifj67ari684p89bcr1kjjh6d5gm46yxyiz9f5qla";
+        };
+      }
+      {
+        name = "zsh-nix-shell";
+        file = "nix-shell.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "chisui";
+          repo = "zsh-nix-shell";
+          rev = "v0.5.0";
+          sha256 = "0za4aiwwrlawnia4f29msk822rj9bgcygw6a8a6iikiwzjjz0g91";
+        };
+      }
+    ];
+  };
+}
