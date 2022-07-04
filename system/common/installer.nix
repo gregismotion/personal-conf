21804cc92dd7b6e13f3f5299ef74613ae116514a
thegergo02 <work@varigergo.xyz>
2022-07-04 17:06:22 +0200

    fix: wait for config

--- system/common/installer.nix
+++ system/common/installer.nix
@@ -6,6 +6,11 @@
     WAIT=5
     echo "Starting default installation in $WAIT seconds..."
     sleep $WAIT
+    until ! [ -f "/etc/nixos/scripts/install.sh" ]
+    do
+      sleep 2
+      echo "Waiting for install script..."
+    done
     /etc/nixos/scripts/install.sh
   '';
 }
