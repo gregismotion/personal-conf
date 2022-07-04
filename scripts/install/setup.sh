ce9430570d6ffc9c34c96140e62484ca709f746d
thegergo02 <work@varigergo.xyz>
2022-07-04 15:12:14 +0200

    fix: remove pwd

--- scripts/install/setup.sh
+++ scripts/install/setup.sh
@@ -5,7 +5,6 @@ if [[ -d "$ROOT" ]]; then
 	echo "$ROOT is created, assuming manual installation..."
 else
 	echo "Missing system root, trying 'setup.sh'."
-	pwd
 	SETUP=system/$HOST/setup.sh
 	if [[ -f "$SETUP" ]]; then
 		echo "Running '$SETUP'..."
