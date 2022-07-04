2744b372dba583ecac2e63c69fd53c22e5b3dd73
thegergo02 <work@varigergo.xyz>
2022-07-04 12:06:45 +0200

    fix: use attribute set

--- users/thegergo02/elems/ssh.nix
+++ users/thegergo02/elems/ssh.nix
@@ -3,31 +3,27 @@
 {
   programs.ssh = {
     enable = true;
-    matchBlocks = [
-      {
-        host = "github";
+    matchBlocks = {
+      github = {
         hostname = "github.com";
-        username = "git";
+        user = "git";
         identityFile = "~/.ssh/id_raspi_git";
-      }
-      {
-        host = "freeself";
+      };
+      freeself = {
         hostname = "freeself.one";
         user = "pi";
         identityFile = "~/.ssh/id_rsa_raspi";
-      }
-      {
-        host = "zeus";
+      };
+      zeus = {
         hostname = "192.168.1.110";
         user = "thegergo02";
         identityFile = "~/.ssh/id_rsa_zeus";
-      }
-      {
-        host = "freeself_git";
+      };
+      freeself_git = {
         hostname = "git.freeself.one";
         user = "git";
         identityFile = "~/.ssh/id_raspi_git";
-      }
-    ];
+      };
+    };
   };
 }
