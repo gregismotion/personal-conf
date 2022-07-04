5824f3f576d75149a75f285ac3bf9aadf1706755
thegergo02 <work@varigergo.xyz>
2022-07-04 15:14:15 +0200

    fix: source scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -12,19 +12,19 @@ source scripts/install/setup.sh
 
 if [[ -d "system/$HOST" ]]; then
 	echo "Generating hardware-configuration.nix for $HOST."
-	scripts/install/generate-hardware-conf.sh
+	source scripts/install/generate-hardware-conf.sh
 	
 	echo "Copying configuration to $ROOT..."
-	scripts/install/copy-conf.sh
+	source scripts/install/copy-conf.sh
 
 	echo "Installing NixOS on $HOST..."
-	scripts/install/install.sh
+	source scripts/install/install.sh
 	
 	echo "Fixing permissions on config directory."
 	$ROOT/etc/nixos/scripts/common/fix-perms.sh
 	
 	echo "Pushing new hardware configuration..."
-	scripts/install/push-hardware-conf.sh
+	source scripts/install/push-hardware-conf.sh
 	
 	echo "NixOS is installed and configured on $HOST."
 	echo "Veni, vidi, vici."
