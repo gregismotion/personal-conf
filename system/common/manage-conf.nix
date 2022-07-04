ecce32210374b50781268af53b2a9642511d2b3d
thegergo02 <work@varigergo.xyz>
2022-07-04 13:40:16 +0200

    fix: copy hidden files

--- system/common/manage-conf.nix
+++ system/common/manage-conf.nix
@@ -18,8 +18,9 @@
 	      esac
       done
       if [[ ! -d "/etc/nixos/.git" ]]; then
-        cp -a ${self}/* /etc/nixos
-        cp -a ${self}/.* /etc/nixos
+        mkdir -p /etc/nixos
+        git clone https://git.freeself.one/thegergo02/personal-conf
+        mv personal-conf/* personal-conf/.* /etc/nixos/.
       fi
       # TODO: apply somewhere (but installer does not need it for example)
 
