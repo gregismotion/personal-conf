7eecc2f6543e74f91e1d422ecbdc8d189ca9f529
thegergo02 <work@varigergo.xyz>
2022-07-04 17:59:49 +0200

    fix: reboot after install

--- system/common/installer.nix
+++ system/common/installer.nix
@@ -6,12 +6,7 @@
     WAIT=10
     echo "Starting default installation in $WAIT seconds..."
     sleep $WAIT
-    #until ! [ -f "/etc/nixos/scripts/install.sh" ]
-    #do
-    #  sleep 2
-    #  echo "Waiting for install script..."
-    #  ls /etc/nixos/scripts
-    #done
     /etc/nixos/scripts/install.sh
+    reboot
   '';
 }
