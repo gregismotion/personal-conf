f9545a9512962b767ea4fc26b0a0b1a3a6f1652c
thegergo02 <work@varigergo.xyz>
2022-07-06 19:48:10 +0200

    refactor: remove unnecessary stuff

--- system/zeus/zfs.nix
+++ system/zeus/zfs.nix
@@ -2,18 +2,6 @@
 {
   networking.hostId = "fc3d22fc"; # NOTE: echo "<hostname>" | md5sum | head -c 8
 
-  /*boot = {
-    initrd.supportedFilesystems = [ "zfs" ];
-    supportedFilesystems = [ "zfs" ];
-  };*/
-  
-  fileSystems = {
-    "/data/torrent".options = [ "noauto" ];
-    "/data/share".options = [ "noauto" ];
-    "/data/postgres".options = [ "noauto" ];
-    "/data/important".options = [ "noauto" ];
-  };
-
   services.zfs = {
     trim.enable = true;
     autoScrub.enable = true;
