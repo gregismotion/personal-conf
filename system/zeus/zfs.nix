c00b17e6d610cbf959f4815f2468d2d59548d598
thegergo02 <work@varigergo.xyz>
2022-07-06 18:51:12 +0200

    add: data to scrub

--- system/zeus/zfs.nix
+++ system/zeus/zfs.nix
@@ -5,7 +5,7 @@
   services.zfs = {
     trim.enable = true;
     autoScrub.enable = true;
-    autoScrub.pools = [ "sys" ]; # FIXME: this can change in setup.sh
+    autoScrub.pools = [ "sys" "data" ]; # FIXME: this can change in setup.sh
     autoSnapshot = {
       enable = true;
       monthly = 2;
