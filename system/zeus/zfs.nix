bc8c2b68da11262dba01f6aa1a523d14dce072e2
thegergo02 <work@varigergo.xyz>
2022-07-06 19:15:02 +0200

    fix: zgs

--- system/zeus/zfs.nix
+++ system/zeus/zfs.nix
@@ -2,6 +2,11 @@
 {
   networking.hostId = "fc3d22fc"; # NOTE: echo "<hostname>" | md5sum | head -c 8
 
+  boot = {
+    initrd.supportedFilesystems = [ "zfs" ];
+    supportedFilesystems = [ "zfs" ];
+  };
+
   services.zfs = {
     trim.enable = true;
     autoScrub.enable = true;
