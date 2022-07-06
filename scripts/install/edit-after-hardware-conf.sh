cd2426e527a66555b5061d3b16f0b7ea8cd0ed83
thegergo02 <work@varigergo.xyz>
2022-07-06 15:44:26 +0200

    fix: check var

--- scripts/install/edit-after-hardware-conf.sh
+++ scripts/install/edit-after-hardware-conf.sh
@@ -1,5 +1,5 @@
-if [ -z "${EDIT_AFTER_HARDWARE_CONF+x}" ]; then
-	echo "EDIT_AFTER_HARDWARE_CONF not set, continuing..."
-else
+if [[ -v EDIT_AFTER_HARDWARE_CONF ]]; then
 	$EDITOR $EDIT_AFTER_HARDWARE_CONF
+else
+	echo "EDIT_AFTER_HARDWARE_CONF not set, continuing..."
 fi
