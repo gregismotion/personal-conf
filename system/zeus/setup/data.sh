e137cf61a2468e48ed5e921a89963edc9da69767
thegergo02 <work@varigergo.xyz>
2022-07-06 17:10:22 +0200

    refactor: use legacy mountpoints

--- system/zeus/setup/data.sh
+++ system/zeus/setup/data.sh
@@ -17,15 +17,18 @@ zfs create -o refreservation=$RESERVED_SIZE -o mountpoint=none $DATA_POOL/reserv
 zfs create $DATA_LOCAL_POOL
 zfs create \
 	-o recordsize=16KB \
-	-o canmount=on \
-	-o mountpoint=/data/torrent \
+	-o mountpoint=legacy \
 	${DATA_LOCAL_POOL}/torrent
+mkdir -p $ROOT/data/torrent
+mount -t zfs ${DATA_LOCAL_POOL}/torrent $ROOT/data/torrent
+
 zfs create \
 	-o recordsize=1M \
-	-o canmount=on \
-	-o mountpoint=/data/share \
+	-o mountpoint=legacy \
 	-o dedup=on \
 	${DATA_LOCAL_POOL}/share
+mkdir -p $ROOT/data/share
+mount -t zfs ${DATA_LOCAL_POOL}/share $ROOT/data/share
 
 zfs create $DATA_SAFE_POOL
 zfs set com.sun:auto-snapshot=true $DATA_SAFE_POOL
@@ -33,12 +36,15 @@ zfs create \
 	-o redundant_metadata=most \
 	-o recordsize=16k \
 	-o logbias=throughput \
-	-o canmount=on \
-	-o mountpoint=/data/postgres \
+	-o mountpoint=legacy \
 	${DATA_SAFE_POOL}/postgres
+mkdir -p $ROOT/data/postgres
+mount -t zfs ${DATA_SAFE_POOL}/postgres $ROOT/data/postgres
+
 zfs create \
 	-o recordsize=1M \
-	-o canmount=on \
-	-o mountpoint=/data/important \
+	-o mountpoint=legacy \
 	-o dedup=on \
 	${DATA_SAFE_POOL}/important
+mkdir -p $ROOT/data/important
+mount -t zfs ${DATA_SAFE_POOL}/important $ROOT/data/important
--- system/zeus/setup/sys.sh
+++ system/zeus/setup/sys.sh
@@ -16,17 +16,22 @@ zpool create -f \
 zfs create -o refreservation=$RESERVED_SIZE -o mountpoint=none $SYS_POOL/reserved
 
 zfs create $SYS_LOCAL_POOL
+
 zfs create \
 	-o xattr=sa \
 	-o acltype=posixacl \
-	-o canmount=on \
-	-o mountpoint=/var \
+	-o mountpoint=legacy \
 	-o dedup=on \
 	${SYS_LOCAL_POOL}/var
+mkdir -p $ROOT/var
+mount -t zfs ${SYS_LOCAL_POOL}/var $ROOT/var
+
 zfs create \
 	-o compression=off \
 	-V $SWAP_SIZE \
 	${SYS_LOCAL_POOL}/swap
+mkswap -f -L swap ${SYS_LOCAL_POOL}/swap
+swapon ${SYS_LOCAL_POOL}/swap
 
 zfs create $SYS_SAFE_POOL
 zfs set com.sun:auto-snapshot=true $SYS_SAFE_POOL
@@ -35,13 +40,21 @@ zfs create \
 	-o mountpoint=/nix \
 	-o dedup=on \
 	${SYS_SAFE_POOL}/nix
+mkdir -p $ROOT/nix
+mount -t zfs ${SYS_SAFE_POOL}/nix $ROOT/nix
+
 zfs create \
 	-o canmount=on \
 	-o mountpoint=/home \
 	-o dedup=on \
 	${SYS_SAFE_POOL}/home
+mkdir -p $ROOT/home
+mount -t zfs ${SYS_SAFE_POOL}/home $ROOT/home
+
 zfs create \
 	-o canmount=on \
 	-o mountpoint=/persist \
 	-o dedup=on \
 	${SYS_SAFE_POOL}/persist
+mkdir -p $ROOT/persist
+mount -t zfs ${SYS_SAFE_POOL}/persist $ROOT/persist
