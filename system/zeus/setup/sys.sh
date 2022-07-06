c6106f3bc69f28decb74531ede0e9a4b964b24aa
thegergo02 <work@varigergo.xyz>
2022-07-06 18:24:30 +0200

    refactor: uefi boot

--- system/zeus/setup/sys.sh
+++ system/zeus/setup/sys.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+sfdisk $SYS_DISK < $PREFIX/setup/sys.dump
+
+BOOT_PART=${SYS_DISK}-part1
+mkfs.vfat -F 32 -n boot $BOOT_PART
+mkdir -p $ROOT/boot
+mount $BOOT_PART $ROOT/boot
+
+SWAP_PART=${SYS_DISK}-part2
+mkswap -f -L swap $SWAP_PART
+swapon $SWAP_PART
+
 zpool create -f \
 	-O compression=lz4 \
 	-o ashift=12 \
@@ -10,7 +21,7 @@ zpool create -f \
 	-O dnodesize=auto \
 	-O normalization=formD \
 	-o autotrim=on \
-	$SYS_POOL $SYS_DISK
+	$SYS_POOL ${SYS_DISK}-part3
 
 # NOTE: needed 'cos COW
 zfs create -o refreservation=$RESERVED_SIZE -o mountpoint=none $SYS_POOL/reserved
@@ -25,8 +36,6 @@ zfs create \
 	${SYS_LOCAL_POOL}/var
 mkdir -p $ROOT/var
 mount -t zfs ${SYS_LOCAL_POOL}/var $ROOT/var
-
-# TODO: swap
 
 zfs create $SYS_SAFE_POOL
 zfs set com.sun:auto-snapshot=true $SYS_SAFE_POOL
