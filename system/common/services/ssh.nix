b262ec22462651be1d99491e5454879cef7e71f2
thegergo02 <work@varigergo.xyz>
2022-07-07 18:20:08 +0200

    fix: persistent ssh host keys

--- system/common/services/ssh.nix
+++ system/common/services/ssh.nix
@@ -8,6 +8,17 @@
       passwordAuthentication = false;
       permitRootLogin = "no";
       challengeResponseAuthentication = false;
+      hostKeys = [
+        {
+          path = "/persist/etc/ssh/ssh_host_ed25519_key";
+          type = "ed25519";
+        }
+        {
+          path = "/persist/etc/ssh/ssh_host_rsa_key";
+          type = "rsa";
+          bits = 4096;
+        }
+      ];
       extraConfig = ''
         AllowGroups ssh
       '';
