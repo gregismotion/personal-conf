e3a1197a7d2ba9fbea9ba7f8b640f8aa1648a2df
thegergo02 <work@varigergo.xyz>
2022-07-07 13:46:21 +0200

    fix: use github if freeself down

--- users/root/elems/git.nix
+++ users/root/elems/git.nix
@@ -7,6 +7,7 @@
     userEmail = "root@freeself.one";
     extraConfig = {
       url."freeself_git:".insteadOf = "https://git.freeself.one/";
+      url."github:".insteadOf = "https://github.com/";
     };
   };
 }
