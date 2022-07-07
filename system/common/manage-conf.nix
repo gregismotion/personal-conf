c92f42b525072f272ab037c691e37cfa8f5a6073
thegergo02 <work@varigergo.xyz>
2022-07-07 15:21:34 +0200

    fix: manage-conf

--- system/common/manage-conf.nix
+++ system/common/manage-conf.nix
@@ -4,27 +4,26 @@
   imports = [ groups/conf.nix ];
   systemd.services.manage-conf = {
     script = ''
-      mkdir -p /persist/nixos
-
+      mkdir -p /persist
       # FIXME: hardcoded paths
       if [[ ! -d "/persist/nixos/.git" ]]; then
         rm -rf /persist/nixos
-        cp -r ${self}/ nixos
+        cp -r ${self}/ /persist/nixos
       fi
       cp -r ${inputs.keys}/ /persist/nixos/keys
       pushd /persist/nixos
-        #git init
-        #git remote add origin https://git.freeself.one/thegergo02/personal-conf
-        #git remote add github https://github.com/thegergo02/personal-conf
+        git init
+        git remote add origin-auto https://git.freeself.one/thegergo02/personal-conf
+        git remote add github-auto https://github.com/thegergo02/personal-conf
         git add .
         until curl -s -f -o /dev/null "https://nixos.org"
         do
           sleep 5
         done
         git reset --hard
-        git pull -f --set-upstream origin master
+        git pull --set-upstream origin-auto master
         if [[ $? != 0 ]]; then
-          git pull -f github
+          git pull github-auto
         fi
       popd
     '';
@@ -33,7 +32,8 @@
     after = [ "network-online.target" ];
     wants = [ "network-online.target" "systemd-networkd-wait-online.service" ];
     serviceConfig.Type = "oneshot";
-    serviceConfig.WorkingDirectory = "/persist";
+    serviceConfig.RuntimeDirectory = "/persist";
+    serviceConfig.PermissionsStartOnly = "true";
     path = [ pkgs.git pkgs.curl ];
   };
 }
