846e6d48ae6800117d2a92a6ab915eeac013cd16
thegergo02 <work@varigergo.xyz>
2022-07-09 14:44:26 +0200

    fix: zitadel

--- system/common/services/zitadel.nix
+++ system/common/services/zitadel.nix
@@ -5,7 +5,8 @@
     enable = true;
     masterKey = "p1Au595d5UKy6nPC802FFfpn8Kgc6wuV";
     extraConfig = ''
-    ExternalSecure: false
+      ExternalSecure: false
+      tlsMode: "disable"
     '';
   };
   config.services.cockroachdb22.enable = true;
