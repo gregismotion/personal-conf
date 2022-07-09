0ecf7521d5e708e739aac8d61e43ed5c3d22fb64
thegergo02 <work@varigergo.xyz>
2022-07-09 14:43:11 +0200

    fix: zitadel

--- system/common/services/zitadel.nix
+++ system/common/services/zitadel.nix
@@ -4,6 +4,9 @@
   config.services.zitadel = {
     enable = true;
     masterKey = "p1Au595d5UKy6nPC802FFfpn8Kgc6wuV";
+    extraConfig = ''
+    ExternalSecure: false
+    '';
   };
   config.services.cockroachdb22.enable = true;
 }
