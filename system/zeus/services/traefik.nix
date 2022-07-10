cb58b4a8af682e04127c578447529c22d667b135
thegergo02 <work@varigergo.xyz>
2022-07-10 18:57:56 +0200

    fix: traefik

--- system/zeus/services/traefik.nix
+++ system/zeus/services/traefik.nix
@@ -6,28 +6,43 @@
   config.services.traefik = {
     dataDir = "/persist/traefik/";
     staticConfigOptions = {
-      entrypoints.web.address = ":80";
-      entrypoints.websecure.address = ":443";
+      entrypoints = {
+        web.address = ":80";
+        websecure.address = ":443";
+      };
 
-      certificatesresolvers.le.acme.email = "varigergo05@gmail.com";
-      certificatesresolvers.le.acme.dnschallenge.provider = "njalla";
-      certificatesresolvers.le.acme.dnschallenge.resolvers = "1.1.1.1:53,8.8.8.8:53";
-      certificatesresolvers.le.acme.storage = "acme.json";
+      certificatesresolvers.le.acme = {
+        email = "varigergo05@gmail.com";
+        dnschallenge = {
+          provider = "njalla";
+          resolvers = "1.1.1.1:53,8.8.8.8:53";
+        };
+        storage = "acme.json";
+      };
 
       api.dashboard = true;
 
-      #traefik.http.routers.traefik.tls = "true";
-      traefik.http.routers.traefik.tls.certresolver = "le";                                                     traefik.http.routers.traefik.tls.domains = [ {
-        main = "freeself.one"; 
-        sans = "*.freeself.one";
-      } ];
-      traefik.http.routers.traefik.service = "api@internal";
-
-      traefik.http.middlewares.redirect-to-https.redirectscheme.scheme = "https";
-      traefik.http.routers.redirs.rule = "hostregexp(`{host:.+}`)";
-      traefik.http.routers.redirs.entrypoints = "web";
-      traefik.http.routers.redirs.middlewares = "redirect-to-https";
-      traefik.http.routers.traefik.rule = "Host(`traefik.freeself.one`)";
+      traefik.http = {
+        routers = {
+          traefik = {
+            rule = "Host(`traefik.freeself.one`)";
+            tls = {
+              certresolver = "le";
+              domains = [ {
+                main = "freeself.one"; 
+                sans = "*.freeself.one";
+              } ];
+            };
+            service = "api@internal";
+          };
+          redirs = {
+            rule = "hostregexp(`{host:.+}`)";
+            entrypoints = "web";
+            middlewares = "redirect-to-https";
+          };
+        };
+        middlewares.redirect-to-https.redirectscheme.scheme = "https";
+      };
     };
   }; 
 }
