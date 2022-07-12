a8663ffbeccb69834adf7093607cd8ba1c743e42
thegergo02 <work@varigergo.xyz>
2022-07-12 11:28:03 +0200

    fix: samba

--- system/common/services/samba.nix
+++ system/common/services/samba.nix
@@ -31,6 +31,7 @@
         max protocol = smb3
         hosts allow = 192.168.0. 127.0.0.1 localhost
         hosts deny = 0.0.0.0/0
+        guest account = root
         map to guest = bad user
       '';
       shares = {
