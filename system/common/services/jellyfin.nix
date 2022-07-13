bb4d9bc691d479bcddd51f250374441b625c631d
thegergo02 <work@varigergo.xyz>
2022-07-13 15:50:00 +0200

    add: jellyfin

--- system/common/services/jellyfin.nix
+++ system/common/services/jellyfin.nix
@@ -10,6 +10,6 @@
   systemd.services.jellyfin.serviceConfig = {
     StateDirectory = lib.mkForce "/data/persist/jellyfin/state";
     CacheDirectory = lib.mkForce "/data/persist/jellyfin/cache";
-    ExecStart = "${config.services.jellyfin.package}/bin/jellyfin --datadir '${config.systemd.services.jellyfin.serviceConfig.StateDirectory}' --cachedir '${config.systemd.services.jellyfin.serviceConfig.CacheDirectory}'";
+    ExecStart = "${config.services.jellyfin.package}/bin/jellyfin --datadir '/data/persist/jellyfin/state' --cachedir '/data/persist/jellyfin/cache'";
   };
 }
