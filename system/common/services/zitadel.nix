361f644f774882f8611427f5eb425d3ec8ff61e7
thegergo02 <work@varigergo.xyz>
2022-07-14 16:35:26 +0200

    fix: zitadel

--- system/common/services/zitadel.nix
+++ system/common/services/zitadel.nix
@@ -15,7 +15,7 @@
       extraCommand = "--tlsMode external --steps ${config.age.secrets.services-zitadel-admin.path} --masterkeyFile ${config.age.secrets.services-zitadel-masterkey.path}";
     };
     systemd.services.zitadel.serviceConfig.After = "cockroachdb22.service";
-    systemd.services.cockroachdb22.serviceConfig.ExecStartPost="/usr/bin/timeout 30 sh -c 'while ! ss -H -t -l -n sport = :26257 | grep -q "^LISTEN.*:61616"; do sleep 1; done'";
+    systemd.services.cockroachdb22.serviceConfig.ExecStartPost="/usr/bin/timeout 30 sh -c 'while ! ss -H -t -l -n sport = :26257 | grep -q \"^LISTEN.*:26257\"; do sleep 1; done'";
     services.cockroachdb22 = {
       enable = true;
       workingDirectory = /data/postgres/cockroach;
