adfa0196b78f4b071bf2cf957243cb324e2d7ac8
thegergo02 <work@varigergo.xyz>
2022-07-14 16:50:35 +0200

    fix: zitadel

--- system/common/services/zitadel.nix
+++ system/common/services/zitadel.nix
@@ -21,7 +21,7 @@
     };
     systemd.services.zitadel.serviceConfig.After = "cockroachdb22.service";
     systemd.services.cockroachdb22 = {
-      socketOptions = {
+      socketConfig = {
         ListenStream = "26257";
         Accept = false;
       };
