f02b98bafb262d89bf485dee2dd69817c8ec231f
thegergo02 <work@varigergo.xyz>
2022-07-14 16:31:28 +0200

    fix: zitadel

--- system/common/services/zitadel.nix
+++ system/common/services/zitadel.nix
@@ -2,20 +2,22 @@
 
 {
   imports = [ "${inputs.secrets}/services/zitadel/zitadel.nix" ];
-  config.services.zitadel = {
-    enable = true;
-    extraConfig = ''
-      ExternalSecure: true
-      ExternalDomain: sso.freeself.one
-      ExternalPort: 443
-      S3DefaultInstance:
-        CustomDomain: sso.freeself.one
-    '';
-    extraCommand = "--tlsMode external --steps ${config.age.secrets.services-zitadel-admin.path} --masterkeyFile ${config.age.secrets.services-zitadel-masterkey.path}";
-  };
-  config.systemd.services.zitadel.serviceConfig.After = "cockroachdb22";
-  config.services.cockroachdb22 = {
-    enable = true;
-    workingDirectory = /data/postgres/cockroach;
+  config = {
+    services.zitadel = {
+      enable = true;
+      extraConfig = ''
+        ExternalSecure: true
+        ExternalDomain: sso.freeself.one
+        ExternalPort: 443
+        S3DefaultInstance:
+          CustomDomain: sso.freeself.one
+      '';
+      extraCommand = "--tlsMode external --steps ${config.age.secrets.services-zitadel-admin.path} --masterkeyFile ${config.age.secrets.services-zitadel-masterkey.path}";
+    };
+    systemd.services.zitadel.serviceConfig.After = "cockroachdb22";
+    services.cockroachdb22 = {
+      enable = true;
+      workingDirectory = /data/postgres/cockroach;
+    };
   };
 }
