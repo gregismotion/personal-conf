c39e962ee9463dd318c22892213147242c4325b0
thegergo02 <work@varigergo.xyz>
2022-07-15 18:36:10 +0200

    add: transmission

--- system/common/services/transmission.nix
+++ system/common/services/transmission.nix
@@ -10,12 +10,9 @@
     enable = true;
     settings = {
       rpc-whitelist = "127.0.0.1";
-
       download-dir = "/data/torrent/complete";
-
       incomplete-dir = "/data/torrent/incomplete";
       incomplete-dir-enabled = true;
-
     };
     home = "/persist/transmission";
   };
