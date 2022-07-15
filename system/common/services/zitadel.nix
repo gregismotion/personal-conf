36ddd7c754fc3e8b69ff6beba23a598e1405fd7e
thegergo02 <work@varigergo.xyz>
2022-07-15 09:36:41 +0200

    fix: zitadel

--- system/common/services/zitadel.nix
+++ system/common/services/zitadel.nix
@@ -22,7 +22,7 @@
     
     systemd.services.zitadelAndCockroach = {
       enable = true;
-      script = with config.systemd.services.cockroachdb22.serviceConfig.ExecStart + config.systemd.services.zitadel.serviceConfig.ExecStart;
+      script = config.systemd.services.cockroachdb22.serviceConfig.ExecStart + config.systemd.services.zitadel.serviceConfig.ExecStart;
     };
 
     services.cockroachdb22 = {
