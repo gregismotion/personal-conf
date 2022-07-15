95418ef8b10d619ec52d3b6a1a936323dbb020e8
thegergo02 <work@varigergo.xyz>
2022-07-15 18:39:51 +0200

    add: transmission

--- system/common/services/transmission.nix
+++ system/common/services/transmission.nix
@@ -10,6 +10,8 @@
     enable = true;
     settings = {
       rpc-whitelist = "127.0.0.1";
+      rpc-host-whitelist = "transmission.freeself.one";
+      rpc-host-whitelist-enabled = true;
       download-dir = "/data/torrent/complete";
       incomplete-dir = "/data/torrent/incomplete";
       incomplete-dir-enabled = true;
