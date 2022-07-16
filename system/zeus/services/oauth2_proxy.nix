bb59821ebda46d53ed6fcd1c1268f573b86fe36c
thegergo02 <work@varigergo.xyz>
2022-07-16 19:58:31 +0200

    add: oauth2-proxy + traefik

--- system/zeus/services/oauth2_proxy.nix
+++ system/zeus/services/oauth2_proxy.nix
@@ -15,7 +15,10 @@
     ];
     clientID = "170138090634019182@oauth2_proxy";
     clientSecret = "gsCsMF80yiP6cK1iWnBcZevguArodMDIDihLX5NxbWMCRdUpymzvge3mAwnUKazD"; # FIXME: temp secret
-    cookie.secret = "YxrNGItXaEGulQt97qEvurRAzO1MI9OUt_XrZX6UOPw="; # FIXME: temp secret
+    cookie = {
+      secret = "YxrNGItXaEGulQt97qEvurRAzO1MI9OUt_XrZX6UOPw="; # FIXME: temp secret
+      domain = ".freeself.one";
+    };
     email.domains = [ "*" ];
     extraConfig = {
       user-id-claim = "sub";
